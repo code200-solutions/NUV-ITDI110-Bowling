43bfc70863680fd9a09dab10765875d5a7b8c4db
Agent292 <email.georgemoses@292.gmail.com>
2025-09-05 14:23:03 +1100

    scoreframe2

--- src/score.js
+++ src/score.js
@@ -1,17 +1,4 @@
 function ScoreFrame(roll1,roll2) {
-    let score = 0;
-    for (let i = 1; i < 3; i++) {
-        if (i===1) {
-            score+=roll1;
-        }else if (i===2) {
-            score +=roll2;
-        }
-
-
-        
-    }
+    let score = roll1 + roll2;
     return score;
-    }
-
-    const test = score(3,6);
-    console.log(score)
+}
