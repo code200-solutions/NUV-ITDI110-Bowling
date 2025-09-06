d3fb9c1e82e3433b4a79396fe09012ef802c896c
Eugene BEN <vutinamoli1456@gmail.com>
2025-09-05 21:06:39 -0700

    ScoreFrame Functions V1 - V3

---
+++ src/scoreFrameV2.js
@@ -0,0 +1,11 @@
+function scoreFrame (roll1, roll2) {
+    let score = roll1 + roll2;
+    
+    return score;
+}
+
+
+/*
+scoreFrame(2,3)
+OUTPUT ->  5    
+*/
---
+++ src/scoreFrameV3.js
@@ -0,0 +1,13 @@
+function scoreFrame (roll1, roll2) {
+    let score = 0;
+    
+    score += roll1;
+    score += roll2;
+    return score;
+}
+
+
+/*
+scoreFrame(2,3)
+OUTPUT ->  5    
+*/
