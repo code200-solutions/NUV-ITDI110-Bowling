6d11437ea7a5517c1db3b0f62528e8275bb6eff1
steward <steward.ludvaune@univ.edu.vu>
2025-09-08 11:32:19 +1100

    scoreframe 3

---
+++ src/score3.js
@@ -0,0 +1,9 @@
+function scoreFrame(roll1, roll2) {
+  let score = 0;
+  for (let i = 1; i <= 2; i++) {
+    if (i === 1) score += roll1;
+    if (i === 2) score += roll2;
+  }
+  return score;
+}
+console.log(scoreFrame(3, 6));
