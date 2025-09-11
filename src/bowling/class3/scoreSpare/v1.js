27b59a20dc80f42982ed0711c15e967e127df87b
Boris Gbahoue <bgbahoue@gmail.com>
2025-09-11 11:04:42 +1100

    Add: scoreSpare function

--- src/bowling/class3/scoreSpare/v1.js
+++ src/bowling/class3/scoreSpare/v1.js
@@ -5,10 +5,7 @@
  * @param roll2 nb pins down in the second roll
  * @returns returns 10 for a spare, throw an error if not a spare
  */
-function scoreSpare(roll1, roll2) {
-  // throw new Error(`Not a spare`);
-  // return 10;
-}
+const scoreSpare = (roll1, roll2) => (roll1 + roll2 === 10 ? 10 : null);
 
 const score = scoreSpare(3, 7);
 console.log(`Frame rolls: 3,5 => score = ${score}`);
